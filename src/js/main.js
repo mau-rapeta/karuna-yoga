22f01567e0326be2732143be0e2c3abfdfd541b9
Mau <mauriciorapeta147@gmail.com>
2018-11-04 00:00:46 -0600

    seccion contacto y footer termidos, se añadio spyscroll a la navegacion

--- src/js/main.js
+++ src/js/main.js
@@ -16,4 +16,20 @@ $(document).ready(function() {
         }
         prevScrollpos = currentScrollPos;
     }
-})
+
+    // Scroll smooth 
+    $('.scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+    
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 800);
+                return false;
+            }
+        }
+    });
+});
